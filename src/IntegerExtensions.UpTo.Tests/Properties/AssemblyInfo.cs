559c0d397ee76fa71665dec629e07259ddf30b55
Steven Atkinson <steven.atkinson@gmx.com>
2018-02-18 22:32:29 +0000

    Added the visual studio solution with project and tests.

---
+++ src/IntegerExtensions.UpTo.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IntegerExtensions.UpTo.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IntegerExtensions.UpTo.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5adbc17-c97a-4d7c-b1e1-87b4152ba93e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/IntegerExtensions.UpTo.Tests/UpToExtensionTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace IntegerExtensions.UpTo.Tests
+{
+    [TestFixture]
+    [Parallelizable]
+    public class UpToExtensionTests
+    {
+        [Test]
+        [TestCase(0, 5, new[] { 0, 1, 2, 3, 4, 5 })]
+        [TestCase(5, 8, new[] { 5, 6, 7, 8 })]
+        public void UpTo_return_array_for_range(int start, int max, int[] expected)
+        {
+            var result = start.UpTo(max).ToArray();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void UpTo_when_max_is_lower_than_start_returns_empty_array()
+        {
+            const int start = 5;
+            const int max = 4;
+
+            var result = start.UpTo(max).ToArray();
+            var expected = new int[] { };
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void UpTo_when_max_is_the_same_as_start_returns_array_with_one_item()
+        {
+            const int start = 1;
+            const int max = 1;
+
+            var result = start.UpTo(max).ToArray();
+            var expected = new[] { 1 };
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
